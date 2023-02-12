725d2076a23feda22fd0e2328bdb5331b9415d39
jesta510 <35746040+jesta510@users.noreply.github.com>
2023-02-12 13:08:50 -0500

    Monsters and Waypoints

---
+++ Tower defense Collaboration/Assets/MonsterController.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonsterController : MonoBehaviour
+{
+    public float speed;
+    public int health = 20;
+    private Waypoints wpoints;
+    private int waypointIndex = 0;
+
+    void Start()
+    {
+        wpoints = GameObject.FindGameObjectWithTag("Waypoints").GetComponent<Waypoints>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, wpoints.waypoints[waypointIndex].position, speed * Time.deltaTime);
+
+        if(Vector2.Distance(transform.position, wpoints.waypoints[waypointIndex].position) < 0.03f)
+        {
+            waypointIndex++;
+            if(waypointIndex >= wpoints.waypoints.Length)
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+}
