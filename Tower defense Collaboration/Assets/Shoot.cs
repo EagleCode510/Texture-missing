12aa0e00a9b72898dba7932ff1aacd20a4eda764
EagleCode510 <eleagleeyes510@gmail.com>
2023-02-12 19:46:58 -0500

    Quality fixed

--- Tower defense Collaboration/Assets/Shoot.cs
+++ Tower defense Collaboration/Assets/Shoot.cs
@@ -1,15 +1,18 @@
 using System.Collections;
-using System.Collections.Generic;
-using System.Security.Cryptography;
-using System.Threading;
 using UnityEngine;
 
 public class Shoot : MonoBehaviour
 {
-    private int range = 10;
+    public Transform target;
+    public float range = 15f;
 
     void Update()
     {
-        transform.Rotate(0, 0, 100 * Time.deltaTime);
+        
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        
     }
 }
